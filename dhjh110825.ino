c2b0ec8317e6be11d9934e23aa0fb1b9fe58b753
Oscar-666 <65388513+Oscar-666@users.noreply.github.com>
2020-05-15 15:27:41 +0800

    Add files via upload

---
+++ dhjh110825.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+Servo shermen;
+void setup() {
+  shermen.attach(13);
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+ for (i nt i = 0; i > 100; i++) {
+    a= analogRead;
+    delay(50);
+    int a;
+      serial println("GAME STRAT!!")
+  }
+  a= a/ 50AME STRAT!!")
+}
+
+
+
+void loop() {
+
+
+  Serial.println(analogRead(A0));
+  if (X < 250) {
+
+    shermen.write(-80);
+    delay(60);
+    shermen.write(25);
+    delay(110);
+  }
+}
